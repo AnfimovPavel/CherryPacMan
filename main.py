2f209e60fdc6e87bcda50b46c8421b28e824a45c
AnfimovPavel <anfimovpavel2007@gmail.com>
2024-01-27 16:39:57 +0300

    Check_click

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 600, 600
+COORDINATES = []
 
 
 def start_game(screen):
@@ -12,9 +13,17 @@ def start_game(screen):
         text_y = 150 + 150 * i - text.get_height() // 2
         text_w = text.get_width()
         text_h = text.get_height()
+        COORDINATES.append([text_x - 10, text_y - 10, text_w + 20, text_h + 20])
         pygame.draw.rect(screen, (200, 150, 50), (text_x - 10, text_y - 10,
                                                   text_w + 20, text_h + 20))
         screen.blit(text, (text_x, text_y))
+
+
+def check_click(pos):
+    for i in range(3):
+        if COORDINATES[i][0] <= pos[0] <= COORDINATES[i][0] + COORDINATES[i][2] or \
+                COORDINATES[i][1] <= pos[1] <= COORDINATES[i][1] + COORDINATES[i][3]:
+            pass
 
 
 def main():
@@ -26,6 +35,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                check_click(event.pos)
         screen.fill((0, 0, 200))
         start_game(screen)
         pygame.display.flip()
