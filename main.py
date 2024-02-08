d83f6f815a4e2b9b407e7677b236e273422c67ea
AnfimovPavel <anfimovpavel2007@gmail.com>
2024-02-08 23:37:36 +0300

    Redesigned home screen

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
+import sys
+
 import pygame
+import pygame_gui
 
-WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 600, 600
-COORDINATES = []
+pygame.init()
 
+pygame.display.set_caption('Start')
+window_surface = pygame.display.set_mode((800, 600))
 
-def start_game(screen):
-    text_start = ["Начать игру", "Настройки", "Выйти из игры"]
-    font = pygame.font.SysFont("Georgia", 50)
-    for i in range(3):
-        text = font.render(text_start[i], 1, (50, 70, 0))
-        text_x = 300 - text.get_width() // 2
-        text_y = 150 + 150 * i - text.get_height() // 2
-        text_w = text.get_width()
-        text_h = text.get_height()
-        COORDINATES.append([text_x - 10, text_y - 10, text_w + 20, text_h + 20])
-        pygame.draw.rect(screen, (200, 150, 50), (text_x - 10, text_y - 10,
-                                                  text_w + 20, text_h + 20))
-        screen.blit(text, (text_x, text_y))
+background = pygame.Surface((800, 600))
 
+color = (200, 100, 100)
+background.fill(pygame.Color(color))
 
-def check_click(pos):
-    for i in range(3):
-        if COORDINATES[i][0] <= pos[0] <= COORDINATES[i][0] + COORDINATES[i][2] or \
-                COORDINATES[i][1] <= pos[1] <= COORDINATES[i][1] + COORDINATES[i][3]:
-            pass
+manager = pygame_gui.UIManager((800, 600))
 
+settings = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((325, 275), (150, 50)),
+    text='Settings',
+    manager=manager
+)
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode(WINDOW_SIZE)
-    pygame.display.set_caption("My Game")
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                check_click(event.pos)
-        screen.fill((0, 0, 200))
-        start_game(screen)
-        pygame.display.flip()
-    pygame.display.quit()
+play = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((325, 175), (150, 50)),
+    text='Play',
+    manager=manager
+)
 
+exit = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((325, 375), (150, 50)),
+    text='Exit',
+    manager=manager
+)
 
-if __name__ == "__main__":
-    main()
+clock = pygame.time.Clock()
+running = True
+while running:
+    time_delta = clock.tick(60) / 1000.0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == play:
+                    print('play')
+                elif event.ui_element == settings:
+                    print(settings)
+                elif event.ui_element == exit:
+                    sys.exit()
+
+        manager.process_events(event)
+    manager.update(time_delta)
+    window_surface.blit(background, (0, 0))
+    manager.draw_ui(window_surface)
+    pygame.display.update()
